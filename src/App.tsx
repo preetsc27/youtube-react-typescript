a6f4153e196bc1627015a3b9651d01558b57694b
Gammy <preetsc27@gmail.com>
2019-03-30 00:59:35 +0530

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+interface IAppState{
+  age: number
+}
+
+class App extends Component<any, IAppState> {
+
+  constructor(props: any){
+    super(props)
+    this.state = {
+      age: 0
+    }
+
+    this.incrementAge = this.incrementAge.bind(this)
+    this.decrementAge = this.decrementAge.bind(this)
+  }
+
+  incrementAge(){
+    const { age } = this.state
+    this.setState({
+      age: age + 1
+    })
+  }
+
+  decrementAge(){
+    const { age } = this.state
+    this.setState({
+      age: age - 1
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       <p>My Age: {this.state.age} </p>
+       <button onClick={this.incrementAge}>+</button>
+       <button onClick={this.decrementAge}>-</button>
       </div>
     );
   }
